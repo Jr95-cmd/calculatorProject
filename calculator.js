58bd9dfd86a597c14edb051972af5fcbb227dd61
Jr95-cmd <echarlery@live.com>
2023-02-14 18:31:14 -0400

    functions

---
+++ calculator.js
@@ -0,0 +1,31 @@
+//add function
+function add(){
+    let sum =0; 
+    for(let i=0;i<arguments.length;i++){
+    sum+=arguments[i];
+    };
+    console.log(total);
+    };
+    
+    //subtract function
+    function subtract() {
+        let remainder=arguments[0];
+        for (let i=1; i<arguments.length;i++){
+            remainder-=arguments[i];
+        }
+        console.log(remainder);
+    };
+    
+    //multiply function, this function converts arguments to an array and uses the reduce method 
+    function multiply(... args){
+        return args.reduce(function (a,b){
+            return a*b;
+        });
+    };
+    
+    //divide function
+    function divide(... args){
+        return args.reduce( function (a,b){
+            return a/b;
+        });
+    };
