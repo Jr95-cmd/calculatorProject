2e9c39e44c41f681496aedd06b6a4a10e6636909
Jr95-cmd <echarlery@live.com>
2023-04-20 04:41:04 -0400

    fully functional operation buttons and visible equations

--- calculator.js
+++ calculator.js
@@ -40,13 +40,17 @@ function add(){
     }
 
 
-//define variables
+//define global variables
 let fValue=[];
 let fNum=0;
 let sValue=[];
 let sNum=0;
-let action='';
+let action=[];
 let ans=0;
+let actionFn='';
+let equation=[];
+let displayEquation='';
+let actionCount=0;
 
 
 
@@ -54,55 +58,114 @@ let ans=0;
     const btn=document.querySelectorAll('button');
     btn.forEach((button) =>{
         button.addEventListener('click',() =>{
+            let fn= document.querySelector('#operation');
+            let p= document.querySelector('#num1');
+            let p2= document.querySelector('#num2');
+            let p3= document.querySelector('#result')
             if(sNum==0 && action==''){
+                p3.textContent='';
                 fValue.push(button.id);
                 fNum=parseFloat(fValue.join(""));
                 console.log(fNum);
-
-                //code to add values to html
-                let p= document.querySelector('#num1');
-                p.textContent= fNum;
-                
+                p2.textContent= fNum;        
             } 
            if(button.id=='+'||button.id=='-'||button.id=='*'||button.id=='/'){
-                action=button.id;
-                console.log(button.id);
-                let fn= document.querySelector('#operation');
-                fn.textContent=action
+               fn.textContent='';
+                equation.push(fNum);
+                console.log(equation);
+                action.push(button.id);
+                console.log(action[actionCount]);
+                equation.push(action[actionCount]);
+                actionCount+=1;
+                console.log(equation);
+                displayEquation=equation.join(" ");
+                fn.textContent=displayEquation;
+                p2.textContent='';
             }
                 
-          else if(fNum!=null  && action!=''){
+          else if(fNum!=null  && action.length>=1){
 
                 sValue.push(button.id);
                 sNum=parseFloat(sValue.join(""));
                 console.log(sNum);
-                let p2= document.querySelector('#num2');
                 p2.textContent=sNum;
-
-            }
-                //execute calculation
-            if (button.id=='='){
-                if(action=='+'){
-                    action=add;
-                }
-                 
-                if(action=='-'){
-                 action=subtract;
-                }
- 
-                 if(action=='*'){
-                     action=multiply;
-                 }
- 
-                 if(action=='/'){
-                     action=divide;
-                 }
-                ans= action(fNum,sNum);
-                console.log(ans);
-                let p3= document.querySelector('#result')
-                p3.textContent=ans;
+                equation.push(sNum);
                 
             }
+            //execute calculation if equal function is selected 
+            if (button.id=='='){
+                if(action[0]=='+'){
+                    actionFn=add;
+                }
+                 
+                if(action[0]=='-'){
+                 actionFn=subtract;
+                }
+ 
+                 if(action[0]=='*'){
+                     actionFn=multiply;
+                 }
+ 
+                 if(action[0]=='/'){
+                     actionFn=divide;
+                 }
+                ans= actionFn(fNum,sNum);
+                console.log(ans);
+                //update values and display results
+                p3.textContent=ans;
+                equation=[];
+                fNum=ans;
+                equation.push(fNum);
+                console.log(equation);
+                sValue=[];
+                action.shift();
+                displayEquation=equation.join(" ");
+                fn.textContent=displayEquation;
+                //reset calculation variables 
+                p3.textContent='';
+                p2.textContent='';
+                equation.shift();
+                actionCount=0;
+               
+            }
+            //execute calculation if operation function is selected more than once 
+            if (action.length>1){
+                if(action[0]=='+'){
+                    actionFn=add;
+                }
+                 
+                if(action[0]=='-'){
+                 actionFn=subtract;
+                }
+ 
+                 if(action[0]=='*'){
+                     actionFn=multiply;
+                 }
+ 
+                 if(action[0]=='/'){
+                     actionFn=divide;
+                 }
+                 //call function and update calculation variables.
+                ans= actionFn(fNum,sNum);
+                console.log(ans);
+                p3.textContent=ans;
+                equation=[];
+                fNum=ans;
+                //update equation 
+                equation.push(fNum);
+                console.log(equation);
+                //reset second number array
+                sValue=[];
+                action.shift();
+                equation.push(action[0]);
+                //display results
+                displayEquation=equation.join(" ");
+                fn.textContent=displayEquation;
+                p3.textContent='';
+                p2.textContent='';
+                //add code to update p2 dom  to answer value when executed    
+            }
+            //add code for 'c' button
         })
        
     });
